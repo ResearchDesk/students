f00b14f74b1e18dce4719bec8ecf24b1620a7607
midhus <midhunmohan24@gmail.com>
2016-12-22 13:09:42 +0530

    intial commit

---
+++ DRV/src/drv/DRV.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package drv;
+
+import Action.FSMAction;
+import FSM.FSM;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author user54
+ */
+public class DRV {
+ public static void testFSM() {
+        try {
+            FSM f = new FSM("src/config/config.xml", new FSMAction() {
+                @Override
+                public boolean action(String curState, String message, String nextState, Object args) {
+                    javax.swing.JOptionPane.showMessageDialog(null, curState + ":" + message +" : " +nextState);
+                    /*
+                     * Here we can implement our login of how we wish to handle an action
+                     */
+                    return true;
+                }
+            });
+           
+            System.out.println(f.getCurrentState());
+            f.ProcessFSM("MOVELEFT");
+            System.out.println(f.getCurrentState());
+            f.ProcessFSM("MOVE");
+            System.out.println(f.getCurrentState());
+            f.ProcessFSM("MOVELEFT");
+            System.out.println(f.getCurrentState());
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(DRV.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(DRV.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(DRV.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        testFSM();
+    }
+    
+}
