f974f27f5a747b6fe3e9f7b59af0a1877fd1ecf7
midhus <midhunmohan24@gmail.com>
2016-12-22 14:32:24 +0530

    commit

--- DRV/src/drv/DRV.java
+++ DRV/src/drv/DRV.java
@@ -8,7 +8,9 @@ package drv;
 
 import Action.FSMAction;
 import FSM.FSM;
+import static drv.MonitorProcess.monioringAutomata;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
@@ -19,26 +21,28 @@ import org.xml.sax.SAXException;
  * @author user54
  */
 public class DRV {
+    public static FSM f ;
  public static void testFSM() {
         try {
-            FSM f = new FSM("src/config/config.xml", new FSMAction() {
+             f = new FSM("src/config/config.xml", new FSMAction() {
                 @Override
                 public boolean action(String curState, String message, String nextState, Object args) {
-                    //javax.swing.JOptionPane.showMessageDialog(null, curState + ":" + message +" : " +nextState);
+                   // javax.swing.JOptionPane.showMessageDialog(null, curState + ":" + message +" : " +nextState);
                     /*
                      * Here we can implement our login of how we wish to handle an action
                      */
                     return true;
                 }
             });
-           
-            System.out.println(f.getCurrentState());
-            f.ProcessFSM("MOVELEFT");
-            System.out.println(f.getCurrentState());
-            f.ProcessFSM("MOVE");
-            System.out.println(f.getCurrentState());
-            f.ProcessFSM("MOVERIGHT");
-            System.out.println(f.getCurrentState());
+            ArrayList monioringAutomata = monioringAutomata();
+           System.out.println(monioringAutomata);
+//            System.out.println(f.getCurrentState());
+//            f.ProcessFSM("MOVELEFT");
+//            System.out.println(f.getCurrentState());
+//            f.ProcessFSM("MOVE");
+//            System.out.println(f.getCurrentState());
+//            f.ProcessFSM("MOVELEFT");
+//            System.out.println(f.getCurrentState());
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(DRV.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SAXException ex) {
--- DRV/src/drv/MonitorProcess.java
+++ DRV/src/drv/MonitorProcess.java
@@ -3,8 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package drv;
+
+import static drv.DRV.f;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -12,11 +17,28 @@ package drv;
  */
 public class MonitorProcess {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static HashMap history = new HashMap();
+    public static HashMap w_tokens = new HashMap();
+    public static HashMap gv0 = new HashMap();
+    public static ArrayList GV = new ArrayList();
+   public static String start = "START";
+      public static   List transition = Arrays.asList("MOVELEFT", "MOVE", "MOVELEFT");
+    public static ArrayList monioringAutomata() {
+        ArrayList fullautomata = new ArrayList();
+        List states = f.getAllStates();
+     
+        fullautomata.add(states);
+        fullautomata.add(start);
+        fullautomata.add(transition);
+        return fullautomata;
     }
-    
+
+    public static void INIT() {
+
+        gv0.put("gstate", start);
+        gv0.put("q",f.ProcessFSM(transition.get(0).toString()) );
+        gv0.put("p_trans", "");
+        gv0.put("tokens", "");
+        GV.add(gv0);
+    }
 }
